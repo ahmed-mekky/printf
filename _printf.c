026e3fdb3ab933e22a4a4e68832ab1aa27b6a238
ahmed-mekky <27medsh3pan@gmail.com>
2023-06-26 01:15:53 +0300

    adding some functions

--- _printf.c
+++ _printf.c
@@ -15,31 +15,29 @@ int _printf(const char *format, ...)
 	while (format[n])
 		n++;
 	va_start(ap, format);
-	for (i = 0; i < n; i++)
+	while (i < n, format[i])
 	{
 		if (format[i] == '%')
 		{
 			i++;
-			if (format[i])
+			switch (format[i])
 			{
-				switch (format[i])
-				{
-				case 'c':
-					t += print_c((int)va_arg(ap, int));
-					break;
-				case 's':
-					t += print_s(va_arg(ap, char *));
-					break;
-				case '%':
-					t += print_ps();
-					break;
-				}
+			case 'c':
+				t += print_c((int)va_arg(ap, int));
+				break;
+			case 's':
+				t += print_s(va_arg(ap, char *));
+				break;
+			case '%':
+				t += print_ps();
+				break;
 			}
 		}
 		else
 		{
 			t += _putchar(format[i]);
 		}
+		i++;
 	}
 	va_end(ap);
 	return (t);
