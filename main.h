22d9a37fcf7bacfe1846676cae8c4a8506f4da99
YousefShata <yossef_shata_a@yahoo.com>
2023-06-24 22:33:53 -0700

    fixed prototype

--- main.h
+++ main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _printf(const char *placeholder, ...);
+int _printf(const char *format, ...);
 int _putchar(int c);
 int _puts(const char *s);
 int print_c(char c);
