894e86be40ff1c2d0f0b51a0a2ed901d959b9a2b
YousefShata <yossef_shata_a@yahoo.com>
2023-06-24 20:04:48 -0700

    Betty style fixed

--- put.c
+++ put.c
@@ -1,19 +1,33 @@
 #include "main.h"
 #include <unistd.h>
 
+/**
+ * _putchar - print char
+ * @c: char to print
+ * Return: 1
+ */
+
 int _putchar(int c)
 {
-	return(write(1, &c, 1));
+	return (write(1, &c, 1));
 }
+
+/**
+ * _puts - puts function
+ * @s: string to print
+ * Return: length of string
+ */
 
 int _puts(const char *s)
 {
-	int i;
+int i;
 
 	i = 0;
-	while(s[i])
+	while (s[i])
 	{
 		_putchar(s[i]);
 		i++;
 	}
+	return (i);
 }
+
